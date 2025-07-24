aabf031e0777725a6d074c46af505be032595729
Sugatraj <sugatraj.2106@gmail.com>
2025-07-24 11:59:28 +0530

    added simple login screen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 function Login() {
-  return (
-    <div>Welcome to login page</div>
-  )
+    const numInputRef = useRef(null)
+
+    function handleClick() {
+        if (numInputRef.current) {
+            const inputValue = numInputRef.current.value;
+            const numValue = numInputRef.current.value;
+            console.log(numValue)
+        }
+
+    }
+    return (
+        <>
+            <p>enter your number here</p>
+            <input type="number" ref={numInputRef} />
+            {/* <p>{numInputRef}</p> */}
+            <button type='submit' onClick={handleClick}>Submit</button>
+        </>
+    )
 }
 
 export default Login
