b158272a47bd29047ab9dbd1ee98221929ab1c2a
Sugatraj <sugatraj.2106@gmail.com>
2025-07-24 15:11:32 +0530

    api hitting correctly

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,53 @@
 import React, { useState, useRef } from 'react'
+import axios from 'axios'
 
 function Login() {
+    const [data, setData] = useState('')
+    const [statusCode, setStatusCode] = useState(null)
+    const [message, setMessage] = useState("ss")
     const numInputRef = useRef(null)
 
     function handleClick() {
-        if (numInputRef.current) {
-            const inputValue = numInputRef.current.value;
-            const numValue = numInputRef.current.value;
-            console.log(numValue)
+
+        const numValue = numInputRef.current?.value;
+
+        if (!numValue) {
+            console.log('no numver entered')
+            return
         }
+
+        axios
+            .post('https://men4u.xyz/v2/common/login',
+                {
+                    mobile: numValue,
+                    app_type: "pos"
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+
+                    },
+
+                })
+            .then(response => {
+                setMessage('Login Successful !')
+
+                setStatusCode(response.status)
+
+
+
+            })
+            .catch(error => {
+              
+                if (error.response) {
+                    setMessage(error.response.data?.detail)
+                    setStatusCode(error.response.status)
+                }
+                else{
+                    setMessage('network error !!!')
+                    setStatusCode(null)
+                }
+            })
 
     }
     return (
@@ -17,6 +56,10 @@ function Login() {
             <input type="number" ref={numInputRef} />
             {/* <p>{numInputRef}</p> */}
             <button type='submit' onClick={handleClick}>Submit</button>
+           
+           {message && (
+            <p>{message} {statusCode && `(Status: ${statusCode})`}</p>
+           )}
         </>
     )
 }
